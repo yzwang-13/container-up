e1096202441d71590133855541bd8060a2d81e3f
WilsonWang <wilsonwang@192-168-1-105.tpgi.com.au>
2023-02-27 15:27:43 +1100

    modify port

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var greetingRouter = require('./routes/greeting');
+var indexRouter = require("./routes/index");
+var greetingRouter = require("./routes/greeting");
 
 var app = express();
+const port = 4000;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/greeting', greetingRouter);
+app.use("/", indexRouter);
+app.use("/greeting", greetingRouter);
+app.listen(port, () => console.log(`RCL API v1.0 listening on port ${port}!`));
 
 module.exports = app;
